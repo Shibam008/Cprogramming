cbf6b3eb745e21dfe18b03d446fa607d06f6764c
Shibam Sadhukhan <sadhukhan799@gmail.com>
2023-08-26 21:53:52 +0530

    new

---
+++ basics/decimal_to_octal.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    int decimal ;
+    printf("enter any decimal number");
+    scanf("%d",&decimal);
+
+    printf("the Octal value of %d is : %o",decimal,decimal);
+
+
+    return 0;
+}
