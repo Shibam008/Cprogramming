d04d4f4aa25ec7e86ecbc6619e9424c7f2744dbd
Shibam Sadhukhan <sadhukhan799@gmail.com>
2023-08-27 22:29:26 +0530

    new commit 4

---
+++ basics/octal_to_hexadecimal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+
+    int oct;
+    printf("enter any octal number : ");
+    scanf("%o", &oct);
+     
+     printf("Hexadecimal value of %o is : %x",oct,oct);
+
+     return 0;
+
+    }
