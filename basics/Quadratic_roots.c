5fca817a0524536c5c5b1e65e1be3c42a9f96270
Shibam Sadhukhan <sadhukhan799@gmail.com>
2023-09-23 11:25:02 +0530

    quadratic updated

--- basics/Quadratic_roots.c
+++ basics/Quadratic_roots.c
@@ -1,24 +1,33 @@
-/*/  
-           ALGORITHM
-step 1 : start 
-step 2 : take inputs a,b,c,d,x1,x2   {here d = square root of discriminant}
-step 3 : x1 = (-b+d)/(2*a)
-step 4 : x2 = (-b-d)/(2*a)
-*/
+#include <math.h>
+#include <stdio.h>
+int main() {
+    float a, b, c, discriminant, root1, root2, realPart, imagPart;
+    printf("Enter coefficients a, b and c: ");
+    scanf("%f %f %f", &a, &b, &c);
 
-#include<stdio.h>
-#include<math.h>
-int main (){
-    double a,b,c,d,x1,x2;
-    printf("Enter a b c :");
-    scanf("%lf %lf %lf",&a,&b,&c);
+    discriminant = b * b - 4 * a * c;
 
-    d = sqrt(b*b-4*a*c);
+    // condition for real and different roots
+    if (discriminant > 0) {
+        printf("roots are are real and different\n");
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("root1 = %.2f and root2 = %.2f", root1, root2);
+    }
 
-    x1 = (-b+d)/(2*a);
-    x2 = (-b-d)/(2*a);
+    // condition for real and equal roots
+    else if (discriminant == 0) {
+        printf("roots are real and equivalent\n");
+        root1 = root2 = -b / (2 * a);
+        printf("root1 = root2 = %.2f;", root1);
+    }
 
-    printf("Roots of this equation are %lf & %lf ",x1,x2);
+    // if roots are not real
+    else {
+        realPart = -b / (2 * a);
+        imagPart = sqrt(-discriminant) / (2 * a);
+        printf("root1 = %.2f+%.2fi and root2 = %.2f-%.2fi", realPart, imagPart, realPart, imagPart);
+    }
+
     return 0;
-}
-
+} 
