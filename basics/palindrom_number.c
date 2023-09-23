f11112b26982d5a285a86c120d6ff545046cc9f0
Shibam Sadhukhan <sadhukhan799@gmail.com>
2023-09-23 10:21:46 +0530

    palindrom

---
+++ basics/palindrom_number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main (){
+    int num,reversed=0,remainder,original;
+    printf("Enter a number : ");
+    scanf("%d",&num);
+    original = num;
+
+    while(num != 0){
+        remainder = num % 10;
+        reversed = reversed*10 + remainder;
+        num = num / 10;    // for skipping last digit.
+    }
+
+    if(original == reversed){
+        printf("%d is a palindrom number",original);
+    }
+    else{
+        printf("%d is not a palindrom number",original);
+    }
+}
