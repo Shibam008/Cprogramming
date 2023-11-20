2d3fb109db9389b7257102093b540d3a71a28537
Shibam Sadhukhan <sadhukhan799@gmail.com>
2023-11-20 23:45:18 +0530

    most important problem

---
+++ arrays/find_second_largest_element.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<limits.h>
+int main(){
+    int arr[]=//{1,2,3,4,5};
+    //{5,4,3,2,1};
+    {1,2,4,4,5};
+    int size = sizeof(arr)/sizeof(int);
+    int max = INT_MIN, smax; //smax = second max
+    for(int i=0; i<size; i++){
+        if(max < arr[i]){
+            smax=max; // smax is now previous max
+            max=arr[i]; // max is now new max
+            
+        }
+        // (max > arr[i])
+        else if(smax < arr[i] && max != arr[i]){ 
+            smax = arr[i];
+        }
+    }
+    printf("%d",smax);
+    return 0;
+}
