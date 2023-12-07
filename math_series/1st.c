19cca00a1bf3396c50a194583af68491321d9eb4
Shibam Sadhukhan <sadhukhan799@gmail.com>
2023-12-07 13:43:15 +0530

    modified

--- math_series/1st.c
+++ math_series/1st.c
@@ -13,7 +13,7 @@ int main() {
     for(i=1; i<=(n-1); i++){
         sum = sum + (i/pow(x, i+1));
         }
-        printf("ans of the series is = %.2f",sum+1); 
+        printf("ans of the series is = %.4f",sum+1); 
 //as we need to add 1 for one time , we add that 1 outside the loop with ans and for that reason we iterate the loop n-1 times.
     return 0;
 }
